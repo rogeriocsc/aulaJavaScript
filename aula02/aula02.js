8962f1fc81c9c93dcbbbf6e5bc58f4d186ab3956
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-12 20:23:51 -0300

    Atualização aula02 js

--- aula02/aula02.js
+++ aula02/aula02.js
@@ -7,3 +7,14 @@ res = n1 + n2 * 2
 console.log(res)
 // 20 + (10 * 2)
 // res = 40
+/* 
+Operadores Matemáticos
++ Soma
+- Subtração
+/ Divisão
+* Multiplicação
+% Resto de Divisão
+++ Incremento
+-- Decremento
+
+*/
