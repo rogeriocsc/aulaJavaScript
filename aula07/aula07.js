920aaadc8317c3b24eaeeaf456f0e8224174e722
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-12 20:59:25 -0300

    Criado index aula07 js

---
+++ aula07/aula07.js
@@ -0,0 +1,27 @@
+let n1, n2, n3, n4
+n1 = 12
+n2 = 4
+n3 = 15
+n4 = 2
+console.log((n1 > n2) && (n1 > n3))
+
+/*
+Operadores Lógicos
+&& - and - e
+|| - or - ou
+! - not - não
+*/
+/*
+Tabela Verdade
+AND
+V - V = V
+V - F = F
+F - V = F
+F - F = F
+
+OR
+V - V = V
+V - F = V
+F - V = V
+F - F = F
+*/
