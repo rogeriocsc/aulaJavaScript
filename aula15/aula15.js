175baced2844351700c2db1dc0e8af4e71c70b8f
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 16:37:50 -0300

    Adicionado aula15 js

---
+++ aula15/aula15.js
@@ -0,0 +1,18 @@
+function nome() {
+    return "Rogerio"
+}
+console.log(nome())
+console.log(nome())
+console.log(nome())
+// Função que quando chama ele retorna uma string para o console log
+
+function operacao() {
+    let n1 = 10
+    let n2 = 5
+    let res = n1 * n2
+    return res
+}
+let num = operacao()
+console.log(num)
+
+// Temos um processamento e no final temos um retorno resultado
