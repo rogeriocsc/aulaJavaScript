3a333780390b4acd9ab6cf70a2336d3e1aa0c467
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 17:03:07 -0300

    Adicionado aula 16js

---
+++ aula16/aula16.js
@@ -0,0 +1,13 @@
+function nome(p1) {
+    console.log(p1)
+}
+nome("Rogerio")
+// chamou a função soma, passando uma string para a função
+// este valor entrou em p1, ou seja o console log vai imprimir
+// o que veio passado para dentro da função
+
+function soma(n1, n2) {
+    console.log(n1 + n2)
+}
+soma(10,20)
+// imprimi a soma destes dois valores na função soma 
