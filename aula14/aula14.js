1eb183d299e40b1dd89426e0c7c0b4b0821b6fd6
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 00:26:49 -0300

    Criado aula14 js

---
+++ aula14/aula14.js
@@ -0,0 +1,14 @@
+function nome() {
+    console.log("Rogerio")
+}
+nome()
+// Temos a função, executa o bloco e por ultimo a chamada da função
+
+function soma5_12() {
+    let n1 = 5
+    let n2 = 12
+    let soma = n1 + n2
+    console.log(soma)
+}
+soma5_12()
+console.log("Final do Programa!")
