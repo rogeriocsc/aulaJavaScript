3ba49e9b38833760ff5bcc4bfe46c29a3f171f4e
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 00:12:04 -0300

    Criado aula13 js

---
+++ aula13/aula13.js
@@ -0,0 +1,18 @@
+// let n = 0
+// while (n < 10) {
+//     console.log(n)
+//     n++
+// }
+// while (enquanto) n < 10 executa o bloco e incrementar em 1, n++
+// do while
+/*
+while faz primeiro o teste logico, do while executa primeiro o bloco de comando 
+e depois é que vai fazer o teste lógico.
+*/
+let n = 10
+    do {
+        console.log("name")
+        n++
+    } while(n < 10) 
+console.log("Fim do programa")
+
