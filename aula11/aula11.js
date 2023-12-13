e1f47ee91c33e010d2cb92dd33237ab4361950de
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-12 23:05:50 -0300

    Criado aula11 js

---
+++ aula11/aula11.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 10; i++) {
+    console.log("CURSO - valor do i: " + i) 
+}
+
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i + " É PAR")
+    } else {
+        console.log(i + " É ÍMPAR")
+    }
+}
