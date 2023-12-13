09f1cf6bdc75cacfd32c5de801129ba1e4eab4c4
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-12 22:47:02 -0300

    Criado aula09.js

---
+++ aula09/aula09.js
@@ -0,0 +1,9 @@
+let num = 5
+if (num > 10) {
+    console.log("Numeral maior que 10")
+} else if (num > 5) {
+    console.log("Numeral esta entre 6 e 10")
+} else {
+    console.log("Numeral menor ou igual a 5")  
+}
+console.log("Final do programa")
