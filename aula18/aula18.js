ef8343f11fe03232426ad523a3fbe20106fe1b77
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 21:26:41 -0300

    Adicionado aula 18 Js

---
+++ aula18/aula18.js
@@ -0,0 +1,7 @@
+const v = function (n1,n2) {
+    return n1 + n2
+}
+console.log(v(8,10))
+
+// A Função anonima só vai ser criada no momento da execução
+// A função anonima não tem um nome, e precisa ser associada a uma variavel, quando chamar a constante a função é executada.
