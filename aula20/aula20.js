f2ed7222be6a0dfd1c819d6fd3aa93109efc0969
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 22:08:41 -0300

    Adicionado aula20 JS

---
+++ aula20/aula20.js
@@ -0,0 +1,7 @@
+const cursos = ['CSS', 'HTML', 'JAVASCRIPT', 'PHP', 'REACT']
+cursos.map((el,i)=>{
+    console.log("Curso: " + el + " - Posição do curso: " + i)
+})
+
+// Temos o elemento e a posição deste elemento
+// Tem a funcionalidade de percorrer um array ou uma coleção. Elemento por elemento de uma coleção.
