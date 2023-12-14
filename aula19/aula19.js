1896f9517a570e7b82eef3b8d054316c0c7ed310
rogeriocsc <rogeriocurtiogithub@gmail.com>
2023-12-13 21:49:04 -0300

    Adicionado aula19 JS

---
+++ aula19/aula19.js
@@ -0,0 +1,15 @@
+// const soma = function(v1,v2) {
+//     return v1 + v2
+// }
+// console.log(soma(10,2))
+const soma=(v1,v2)=>{
+    return v1 + v2
+}
+console.log(soma(10,2))
+
+
+/*
+Temos acima exatamente a mesma função, agora com Arrow Function 
+Na Arrow Function ao invés de Function, usamos a seta
+()=>{}
+*/
